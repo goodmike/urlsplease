37a5bcedaf74d95ebc8be06f0c5841d711000278
Michael Harrison <mh@michaelharrison.ws>
2010-08-17 13:25:00 -0400

    Activates SMTP and action_mailer: forgot password works Adds action_mailer.default_url_options config setting. Sets email messages "from" address

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,6 +27,8 @@ Urlsplease::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  
+  config.action_mailer.default_url_options = { :host => 'urlsplease.com' }
 
   # Enable threaded mode
   # config.threadsafe!
